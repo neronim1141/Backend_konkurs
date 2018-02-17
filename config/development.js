dd31b234e907ff41599e5c5eca222acac8a7d3ea
Kacper Kruczek <neronim66@gmail.com>
2018-02-17 12:36:59 +0100

    config

--- config/development.js
+++ config/development.js
@@ -10,7 +10,9 @@ module.exports = {
   },
 
   mongo: {
-    uri: process.env.MONGODB_DB_URL || 'mongodb://localhost/dollars',
+    uri:
+      process.env.MONGODB_DB_URL ||
+      'mongodb://admin:admin123@devcluster-shard-00-00-isjwh.mongodb.net:27017,devcluster-shard-00-01-isjwh.mongodb.net:27017,devcluster-shard-00-02-isjwh.mongodb.net:27017/test?ssl=true&replicaSet=DevCluster-shard-0&authSource=admin',
     options: {
       useMongoClient: true
     },
