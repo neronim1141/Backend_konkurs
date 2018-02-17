7438c97ac24ef469fd1fbf37166c2ee66184a850
Kacper Kruczek <neronim66@gmail.com>
2018-02-17 14:04:04 +0100

    schooltype

---
+++ api/types/school.js
@@ -0,0 +1,37 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLID
+} = require('graphql');
+
+module.exports = new GraphQLObjectType({
+  name: 'Region',
+  fields: () => {
+    const AddressType = require('../types/adress');
+    const SchoolTypeType = require('../types/schooltype');
+    const PersonType = require('../types/person');
+    const ProvinceType = require('../types/province');
+    const RegionType = require('../types/region');
+    const ClassType = require('../types/class');
+    const AplicationType = require('../types/application');
+    return {
+      id: { type: GraphQLID },
+      name: { type: GraphQLString },
+      schoolType: { type: SchoolTypeType },
+      director: { type: PersonType },
+      province: { type: ProvinceType },
+      region: { type: RegionType },
+      classes: { type: new GraphQLList(ClassType) },
+      aplications: { type: new GraphQLList(AplicationType) },
+      teachers: { type: new GraphQLList(PersonType) },
+      email: { type: GraphQLString },
+      patron: { type: GraphQLString },
+      telephone: { type: GraphQLString },
+      fax: { type: GraphQLString }
+    };
+  }
+});
