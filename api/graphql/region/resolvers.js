91cf1de2081c7b8af1e4aa0b856c0a67d33df918
Unknown <mrbanana97k@gmail.com>
2018-02-17 13:52:28 +0100

    queries-part3

---
+++ api/graphql/region/resolvers.js
@@ -0,0 +1,20 @@
+const User = require('../../schema/user');
+
+//#region Read object
+module.exports.getOne = (parentValue, args) => {
+  return User.findById(args.id).then(res => {
+    // console.log(res);
+    return res;
+  });
+};
+module.exports.getList = (parentValue, args, context) => {
+  // throw 'not logged';
+  return User.findAsync({}, '', {
+    limit: args.first || 0,
+    skip: args.offset || 0
+  }).then(res => {
+    // console.log(res);
+    return res;
+  });
+};
+//#endregion
