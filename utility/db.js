ec8ce38651e341dfd35590d4001001c37ccdb197
Kacper Kruczek <neronim66@gmail.com>
2018-03-01 14:54:03 +0100

    .

--- utility/db.js
+++ utility/db.js
@@ -6,23 +6,10 @@ mongoose.set('debug', config.mongo.debug);
 function open() {
   return new Promise((resolve, reject) => {
     if (config.debug) {
-      let Mockgoose = require('mockgoose').Mockgoose;
-      let mockgoose = new Mockgoose(mongoose);
-
-      mockgoose.helper.setDbVersion('3.4.10');
-      mockgoose
-        .prepareStorage()
-        .then(function() {
-          mongoose.connect(
-            config.mongo.test,
-            config.mongo.options,
-            (err, res) => {
-              if (err) return reject(err);
-              resolve();
-            }
-          );
-        })
-        .catch(reject);
+      mongoose.connect(config.mongo.test, config.mongo.options, (err, res) => {
+        if (err) return reject(err);
+        resolve(res);
+      });
     } else {
       mongoose.connect(config.mongo.uri, config.mongo.options, (err, res) => {
         if (err) return reject(err);
